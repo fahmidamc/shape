38d1d0792bb6d61589276cc79853b134a1a2356b
fahmidamc <fahmiummar@gmail.com>
2021-02-23 14:08:56 -0800

    added area calculation of each shape

--- main.py
+++ main.py
@@ -40,12 +40,27 @@ while True:
             ch = int(input("WHICH SHAPE DO YOU WANT"))
             if ch == 2:
                 print("chose circle")
+                r = int(input("Enter circle's radius length: "))
+                circ_area = 3.14 * r * r
+                print(f"The area of triangle is {circ_area}.")
+
             if ch == 2:
                 print("chose square")
+                s = int(input("Enter square's side length: "))
+                sqt_area = s * s
+                print(f"The area of square is {sqt_area}.")
             if ch == 3:
                 print("chose rectangle")
+                l = int(input("Enter rectangle's length: "))
+                b = int(input("Enter rectangle's breadth: "))
+                rect_area = l * b
+                print(f"The area of rectangle is {rect_area}.")
             if ch == 4:
                 print("choose triangle")
+                h = int(input("Enter triangle's height length: "))
+                b = int(input("Enter triangle's breadth length: "))
+                tri_area = 0.5 * b * h
+                print(f"The area of triangle is {tri_area}.")
             if ch == 5:
                 print("program exiting...")
                 print("program exiting...")
